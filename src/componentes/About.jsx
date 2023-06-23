f834857d66a19a61de4bbb282be5d5d7ed52a066
Fabricio <fabriciochoque11@gmail.com>
2023-06-23 11:41:50 -0400

    finalizado

---
+++ src/componentes/About.jsx
@@ -0,0 +1,7 @@
+export const About = () => {
+    return (
+        <main>
+            <h1> ABOUT </h1>
+        </main>
+    );
+};
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,19 @@
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/" className="link"> HOME </Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className="link"> ABOUT </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard" className="link"> DASHBOARD </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
